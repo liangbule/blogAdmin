f00517d5609bffedb080a4b7c81bc0217ec57994
liangbule <576865892@qq.com>
2022-05-19 21:29:08 +0800

    引入UI库,next

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import "antd/dist/antd.css";
-import "./App.css";
 import { Button, DatePicker, version } from "antd";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <DatePicker />
         <Button type="primary" style={{ marginLeft: 8 }}>
           Primary Button
         </Button>
-      </header>
     </div>
   );
 }
