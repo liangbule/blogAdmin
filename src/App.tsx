d5c489ca12127aca01110c40bed0f7435fc168ef
liangbule <576865892@qq.com>
2022-11-20 17:31:30 +0800

    引用store状态

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import "./style/App.scss"
 import "./style/base.scss"
 import {Provider} from "react-redux"
 import store from "./store"
+
 const App = () => {
-    console.log("store",store)
     return (
         <Provider store={store}>
-             <ReactTable/>
+            <ReactTable/>
         </Provider>
     )
 }
